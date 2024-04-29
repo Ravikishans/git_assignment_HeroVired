0de7764a942674c39fa1a2a5db70294fee52f9af
Ravikishan <ravikishan1996@gmail.com>
2024-04-29 19:44:04 +0530

    rectangle area

--- geo_cal.py
+++ geo_cal.py
@@ -1,3 +1,14 @@
 import math
 class GeometryCalculator:
-    
+    def calculate_rectangle_area(self, length, width):
+
+        return length * width
+
+if __name__ == "__main__":
+
+    calculator = GeometryCalculator()
+    length = 10
+    width = 6
+    print(f"the area of rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length,width)}")
+
+
