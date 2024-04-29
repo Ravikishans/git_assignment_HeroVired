8be1ac64e1eaec2f3b32b13987d027863db72723
Ravikishan <ravikishan1996@gmail.com>
2024-04-29 19:35:16 +0530

    circle area

--- geo_cal.py
+++ geo_cal.py
@@ -1,3 +1,10 @@
 import math
 class GeometryCalculator:
+    def calculate_circle_area(self, radius):
+
+        return math.pi * radius ** 2
     
+if __name__=="__main__":
+    calculator = GeometryCalculator()
+    radius = 5
+    print(f"area of the circle with radius {radius}")    
